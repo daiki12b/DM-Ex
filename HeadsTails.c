39e926a2a21e1257d85c278ba1d03a5f970a75e1
DaikiSaito <s1290177@u-aizu.ac.jp>
2022-07-13 14:44:04 +0900

    Victory message added

--- HeadsTails.c
+++ HeadsTails.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 int main(){
     srand((unsigned int)time(NULL));
-    int a=0;
-    int b=0;
-    int result;
+     int a=0;
+     int b=0;
+     int result;
+     char name[100];
 
-    printf("Tossing a coin...\n");
+     printf("Who are you?\n");
+     printf("> ");
+     scanf("%s",name);
+     printf("Hello, %s!\n",name);
+
+     printf("Tossing a coin...\n");
 
     for(int i=1; i<=3; i++){
         printf("Round %d: ",i);    
         result = rand() % 2;
-
         if(result == 0){
             printf("Heads\n");
             a++;
@@ -25,5 +29,10 @@ int main(){
     }
         printf("Heads: %d, Tails: %d\n",a,b);
 
-    return 0;
+        if(a>b){
+            printf("You won!\n");
+        } else if(a<b){
+            printf("You lose!\n");
+        }    
+return 0;
 }
