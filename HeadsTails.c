28e2cd19532fd70559ce0ab79b062da1dafc9538
DaikiSaito <s1290177@u-aizu.ac.jp>
2022-07-13 14:14:40 +0900

    initial revision

---
+++ HeadsTails.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    srand((unsigned int)time(NULL));
+    int a=0;
+    int b=0;
+    int result;
+
+    printf("Tossing a coin...\n");
+
+    for(int i=1; i<=3; i++){
+        printf("Round %d: ",i);    
+        result = rand() % 2;
+
+        if(result == 0){
+            printf("Heads\n");
+            a++;
+        }
+        else if(result == 1){
+            printf("Tails\n");
+            b++;
+        }
+    }
+        printf("Heads: %d, Tails: %d\n",a,b);
+
+    return 0;
+}
